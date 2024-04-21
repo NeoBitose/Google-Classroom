47c37a77e04c57cb0b1e002fe78b8777d85b6d7a
NeoBitose <ramadhanalif430@gmail.com>
2024-04-22 02:07:56 +0700

    initial

---
+++ index.php
@@ -0,0 +1,2 @@
+<?php 
+require 'views/dashboard/index.php';
---
+++ views/dashboard/index.php
@@ -0,0 +1,332 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="/uts/views/asset/img/icon.png" type="image/x-icon">
+  <link rel="stylesheet" href="/uts/views/asset/vendor/bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" href="/uts/views/asset/vendor/fontawesome/css/fontawesome.css">
+  <link rel="stylesheet" href="/uts/views/asset/vendor/fontawesome/css/brands.css">
+  <link rel="stylesheet" href="/uts/views/asset/vendor/fontawesome/css/solid.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" href="/uts/views/asset/css/style.css">
+  <title>Beranda</title>
+</head>
+
+<body>
+  <div class="topbar">
+    <div class="leftbar">
+      <img class="menu-img" src="/uts/views/asset/img/menu.svg" alt="menu">
+      <div class="logo">
+        <img class="logo-img" src="/uts/views/asset/img/icon.png" alt="class-room">
+        <h3 class="">Classroom</h3>
+      </div>
+    </div>
+    <div class="rightbar">
+      <div class="right-icon">
+        <i class="fas fa-plus"></i>
+        <img class="grid" src="/uts/views/asset/img/grid-dot.svg" alt="">
+        <img class="profile-img" src="/uts/views/asset/img/pp.png" alt="">
+      </div>
+    </div>
+  </div>
+  <div class="side-in"></div>
+  <div class="sidebar">
+    <div class="nav scrollarea">
+      <ul>
+        <li class="active">
+          <div class="nav-link-group">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M3.00034 9L12.0003 2L21.0003 9V20C21.0003 20.5304 21.0003 21.5 21.0003 22C20.5003 22 19.5308 22 19.0003 22H5.00034C4.46991 22 3.5 22 3 22C3 21.5 3.00034 20.5304 3.00034 20V9Z"
+                stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+              <path d="M9 22V12H15V22" fill="#5F6368" />
+              <path d="M9 22V12H15V22" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+            <a href="">Beranda</a>
+          </div>
+        </li>
+        <li>
+          <div class="nav-link-group">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+              class="feather feather-calendar">
+              <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+              <line x1="16" y1="2" x2="16" y2="6"></line>
+              <line x1="8" y1="2" x2="8" y2="6"></line>
+              <line x1="3" y1="10" x2="21" y2="10"></line>
+            </svg>
+            <a href="">Kalender</a>
+          </div>
+        </li>
+      </ul>
+      <hr>
+      <ul>
+        <li class="accordion">
+          <svg focusable="false" width="24" height="24" viewBox="0 0 24 24" class="arrow-li">
+            <path d="M10 17l5-5-5-5v10z"></path>
+          </svg>
+          <div class="nav-link-group">
+            <svg focusable="false" width="24" height="24" viewBox="0 0 24 24" class=" NMm5M"><circle cx="17" cy="12.5" r="2.5"></circle>
+              <path d="M17 15.62c-1.67 0-5 .84-5 2.5V20h10v-1.88c0-1.66-3.33-2.5-5-2.5zM10 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0-6c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2zm1.82 9.17c.01 0 0 0 0 0zM10 13c-2.67 0-8 1.34-8 4v3h8v-2H4v-.99c.2-.72 3.3-2.01 6-2.01.6 0 1.22.07 1.82.17h.01l2.07-1.55c-1.39-.41-2.85-.62-3.9-.62z"></path>
+            </svg>
+            <p class="title" href="">Mengajar</p>
+          </div>
+        </li>
+        <div class="panel">
+          <li class="">
+            <div class="nav-link-group">
+              <svg enable-background="new 0 0 24 24" focusable="false" height="24" viewBox="0 0 24 24" width="24"class=" NMm5M">
+                <g><path d="M20,6h-8l-2-2H4C2.9,4,2.01,4.9,2.01,6L2,18c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V8C22,6.9,21.1,6,20,6z M20,18H4V8h16V18z M18,12H6v-2h12V12z M14,16H6v-2h8V16z"></path>
+                  <rect fill="none" height="24" width="24"></rect>
+                </g>
+              </svg>
+              <a href="">Untuk diperiksa</a>
+            </div>
+          </li>
+          <li>
+            <div class="nav-link-group">
+              <div class="pp-class bg-cyan">I</div>
+              <div class="title-class">
+                <div class="desc-class">
+                  <a href="" class="">
+                    Inovasi Education
+                  </a>
+                </div>
+              </div>
+            </div>
+          </li>
+        </div>
+      </ul>
+      <hr>
+      <ul>
+        <li class="accordion">
+          <svg focusable="false" width="24" height="24" viewBox="0 0 24 24" class="arrow-li">
+            <path d="M10 17l5-5-5-5v10z"></path>
+          </svg>
+          <div class="nav-link-group">
+            <svg focusable="false" width="24" height="24" viewBox="0 0 24 24" class=" NMm5M">
+              <path
+                d="M12 3L1 9l4 2.18v6L12 21l7-3.82v-6l2-1.09V17h2V9L12 3zm6.82 6L12 12.72 5.18 9 12 5.28 18.82 9zM17 15.99l-5 2.73-5-2.73v-3.72L12 15l5-2.73v3.72z">
+              </path>
+            </svg>
+            <p class="title" href="">Terdaftar</p>
+          </div>
+        </li>
+        <div class="panel">
+          <li class="">
+            <div class="nav-link-group">
+              <svg focusable="false" height="24" viewBox="0 0 24 24" width="24" class=" NMm5M">
+                <g>
+                  <rect fill="none" height="24" width="24"></rect>
+                </g>
+                <g>
+                  <g>
+                    <path
+                      d="M20,3H4C2.9,3,2,3.9,2,5v14c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V5 C22,3.9,21.1,3,20,3z M20,19H4V5h16V19z"
+                      fill-rule="evenodd"></path>
+                    <polygon fill-rule="evenodd" points="19.41,10.42 17.99,9 14.82,12.17 13.41,10.75 12,12.16 14.82,15">
+                    </polygon>
+                    <rect fill-rule="evenodd" height="2" width="5" x="5" y="7"></rect>
+                    <rect fill-rule="evenodd" height="2" width="5" x="5" y="11"></rect>
+                    <rect fill-rule="evenodd" height="2" width="5" x="5" y="15"></rect>
+                  </g>
+                </g>
+              </svg>
+              <a href="">Daftar tugas</a>
+            </div>
+          </li>
+          <li>
+            <div class="nav-link-group">
+              <div class="pp-class bg-blue">S</div>
+              <div class="title-class">
+                <div class="desc-class">
+                  <p class="title">
+                    Sistem Operasi Kelas C 23/24
+                  </p>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="nav-link-group">
+              <div class="pp-class bg-black">K</div>
+              <div class="title-class">
+                <div class="desc-class">
+                  <p class="title">
+                    KECERDASAN BUATAN B
+                  </p>
+                  <!-- <p>PKN</p> -->
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="nav-link-group">
+              <div class="pp-class bg-cyan">G</div>
+              <div class="title-class">
+                <div class="desc-class">
+                  <p class="title">
+                    GRAFIKA KOMPUTER B
+                  </p>
+                  <!-- <p>PKN</p> -->
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="nav-link-group">
+              <div class="pp-class bg-orange">X</div>
+              <div class="title-class">
+                <div class="desc-class">
+                  <p class="title">
+                    XII RPL
+                  </p>
+                  <p class="desc">PKN</p>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="nav-link-group">
+              <div class="pp-class bg-cyan">X</div>
+              <div class="title-class">
+                <div class="desc-class">
+                  <p class="title">
+                    XII RPL
+                  </p>
+                  <p class="desc">MULOK JAWA</p>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="nav-link-group">
+              <div class="pp-class bg-green">X</div>
+              <div class="title-class">
+                <div class="desc-class">
+                  <p class="title">
+                    XII RPL 2021-2022
+                  </p>
+                  <p class="desc">SEMESTER GANJIL</p>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="nav-link-group">
+              <div class="pp-class bg-blue">E</div>
+              <div class="title-class">
+                <div class="desc-class">
+                  <p class="title">
+                    English Class XII RPL
+                  </p>
+                  <!-- <p>PKN</p> -->
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="nav-link-group">
+              <div class="pp-class bg-black">1</div>
+              <div class="title-class">
+                <div class="desc-class">
+                  <p class="title">
+                    12 RPL 2021
+                  </p>
+                  <!-- <p>PKN</p> -->
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="nav-link-group">
+              <div class="pp-class bg-black">P</div>
+              <div class="title-class">
+                <div class="desc-class">
+                  <p class="title">
+                    PBO (PEMOGRAMAN BERORIENTASI OBJECT)
+                  </p>
+                  <p class="desc">XI RPL</p>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="nav-link-group">
+              <div class="pp-class bg-blue">X</div>
+              <div class="title-class">
+                <div class="desc-class">
+                  <p class="title">
+                    XI RPL(Pemodelan&Basisdata)
+                  </p>
+                  <p class="desc"></p>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="nav-link-group">
+              <div class="pp-class bg-blue">X</div>
+              <div class="title-class">
+                <div class="desc-class">
+                  <p class="title">
+                    XI RPL
+                  </p>
+                  <p class="desc">Tahun Pelajaran 2020/2021</p>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="nav-link-group">
+              <div class="pp-class bg-blue">X</div>
+              <div class="title-class">
+                <div class="desc-class">
+                  <p class="title">
+                    XI RPL 2020
+                  </p>
+                  <p class="desc">Pemrograman web dan Perangkat bergerak</p>
+                </div>
+              </div>
+            </div>
+          </li>
+        </div>
+      </ul>
+      <hr>
+      <ul>
+        <li class="">
+          <div class="nav-link-group">
+            <svg stroke-width="0" focusable="false" width="24" height="24" viewBox="0 0 24 24" class=" NMm5M">
+              <path
+                d="M20.54 5.23l-1.39-1.68C18.88 3.21 18.47 3 18 3H6c-.47 0-.88.21-1.16.55L3.46 5.23C3.17 5.57 3 6.02 3 6.5V19c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V6.5c0-.48-.17-.93-.46-1.27zM6.24 5h11.52l.83 1H5.42l.82-1zM5 19V8h14v11H5zm11-5.5l-4 4-4-4 1.41-1.41L11 13.67V10h2v3.67l1.59-1.59L16 13.5z">
+              </path>
+            </svg>
+            <a href="">Kelas yang diarsipkan</a>
+          </div>
+        </li>
+        <li>
+          <div class="nav-link-group">
+            <svg stroke-width="0" focusable="false" width="24" height="24" viewBox="0 0 24 24" class=" NMm5M">
+              <path
+                d="M13.85 22.25h-3.7c-.74 0-1.36-.54-1.45-1.27l-.27-1.89c-.27-.14-.53-.29-.79-.46l-1.8.72c-.7.26-1.47-.03-1.81-.65L2.2 15.53c-.35-.66-.2-1.44.36-1.88l1.53-1.19c-.01-.15-.02-.3-.02-.46 0-.15.01-.31.02-.46l-1.52-1.19c-.59-.45-.74-1.26-.37-1.88l1.85-3.19c.34-.62 1.11-.9 1.79-.63l1.81.73c.26-.17.52-.32.78-.46l.27-1.91c.09-.7.71-1.25 1.44-1.25h3.7c.74 0 1.36.54 1.45 1.27l.27 1.89c.27.14.53.29.79.46l1.8-.72c.71-.26 1.48.03 1.82.65l1.84 3.18c.36.66.2 1.44-.36 1.88l-1.52 1.19c.01.15.02.3.02.46s-.01.31-.02.46l1.52 1.19c.56.45.72 1.23.37 1.86l-1.86 3.22c-.34.62-1.11.9-1.8.63l-1.8-.72c-.26.17-.52.32-.78.46l-.27 1.91c-.1.68-.72 1.22-1.46 1.22zm-3.23-2h2.76l.37-2.55.53-.22c.44-.18.88-.44 1.34-.78l.45-.34 2.38.96 1.38-2.4-2.03-1.58.07-.56c.03-.26.06-.51.06-.78s-.03-.53-.06-.78l-.07-.56 2.03-1.58-1.39-2.4-2.39.96-.45-.35c-.42-.32-.87-.58-1.33-.77l-.52-.22-.37-2.55h-2.76l-.37 2.55-.53.21c-.44.19-.88.44-1.34.79l-.45.33-2.38-.95-1.39 2.39 2.03 1.58-.07.56a7 7 0 0 0-.06.79c0 .26.02.53.06.78l.07.56-2.03 1.58 1.38 2.4 2.39-.96.45.35c.43.33.86.58 1.33.77l.53.22.38 2.55z">
+              </path>
+              <circle cx="12" cy="12" r="3.5"></circle>
+            </svg>
+            <a href="">Setelan</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <script src="/uts/views/asset/vendor/bootstrap/js/bootstrap.bundle.js"></script>
+  <script src="/uts/views/asset/js/app.js"></script>
+</body>
+
+</html>
